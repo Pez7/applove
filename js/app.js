2e26906e52426a6a65da7fb988cc9607de3799a4
Maria Paz <maria.pazrojasc@gmail.com>
2017-06-01 11:59:29 -0400

    initial comment

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+ (function main() {
+    var boxes = Array.from(document.getElementsByClassName('col-4'));
+    var modal = document.getElementById('col-4-modal');
+    var bodyModal, close, img;
+    boxes.forEach(function(box) {
+        box.addEventListener('click', function() {
+            // Borro todo lo que esta dentro del modal
+            modal.innerHTML = '';
+            bodyModal = document.createElement('div');
+            bodyModal.classList.add('modal-body');
+            bodyModal.innerHTML = box.innerHTML;
+
+            modal.appendChild(bodyModal);
+            modal.classList.remove('hide');
+
+            close = document.createElement('div');
+            close.classList.add('close');
+
+            img = document.createElement('img');
+            img.setAttribute('src', 'http://tojaeurope.com/gallery/Close-icon.png');
+
+            close.appendChild(img);
+            modal.appendChild(close);
+
+            close.addEventListener('click', function() {
+                modal.classList.add('hide');
+            });
+
+        });
+    });
+
+    var close = document.querySelector('#box-services-modal .close');
+    close.addEventListener('click', function() {
+        modal.classList.add('hide');
+    });
+
+
+})();
